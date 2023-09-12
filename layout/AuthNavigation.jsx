b02760be711219b882bdf3313a31816fa719c5d9
jeff0518 <cycivs07@icloud.com>
2023-09-13 02:29:28 +0800

    feat:新增登入組件

--- layout/AuthNavigation.jsx
+++ layout/AuthNavigation.jsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+
+import style from "./AuthNavigation.module.scss";
+
 function AuthNavigation() {
   return (
-    <>
-      <div></div>
-    </>
+    <div className={style.buttonBox}>
+      <div className={style.buttonImg}>
+        <Image
+          src="/images/icon/login.png"
+          width={32}
+          height={32}
+          alt="這是login"
+        />
+      </div>
+    </div>
   );
 }
 
--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -1,10 +1,18 @@
 import Logo from "./Logo";
+import AuthNavigation from "./AuthNavigation";
+
+import style from "./Layout.module.scss";
 
 function Layout() {
   return (
-    <>
-      <Logo />
-    </>
+    <div className={style.container}>
+      <div className={style.logo}>
+        <Logo />
+      </div>
+      <div className={style.AuthNavigation}>
+        <AuthNavigation />
+      </div>
+    </div>
   );
 }
 
