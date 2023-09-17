23c9dee1bc732b5efaaa23670f55138015de373c
jeff0518 <cycivs07@icloud.com>
2023-09-17 23:44:56 +0800

    fix: 修改searchBar

--- components/map/SearchBar.jsx
+++ components/map/SearchBar.jsx
@@ -6,17 +6,20 @@ function SearchBar() {
   return (
     <>
       <div className={style.container}>
-        <div className={style.inputBox}>
-          <InputUI
-            inputId="search__destination"
-            inputStyle="search"
-            inputType="text"
-            inputPlaceholder="目的地"
-          />
+        <div className={style.container_box}>
+          <div className={style.inputBox}>
+            <InputUI
+              inputId="search__destination"
+              inputStyle="search"
+              inputType="text"
+              inputPlaceholder="目的地"
+            />
+          </div>
+          <div className={style.buttonBox}>
+            <ButtonUI text="搜索" btnStyle="btn__pill"></ButtonUI>
+          </div>
         </div>
-        <div className={style.buttonBox}>
-          <ButtonUI text="搜索" btnStyle="btn__pill"></ButtonUI>
-        </div>
+
         {/* 資訊顯示欄位並附上刪除資料按鈕 */}
       </div>
     </>
