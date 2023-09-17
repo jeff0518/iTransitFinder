2463b7d8e49b234d7d20fbcdaba0733b822b76db
jeff0518 <cycivs07@icloud.com>
2023-09-18 02:36:26 +0800

    feat: searchBar資訊欄建立

--- components/map/SearchBar.jsx
+++ components/map/SearchBar.jsx
@@ -1,8 +1,12 @@
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { BiNavigation } from "react-icons/bi";
+
 import ButtonUI from "../shared/ButtonUI";
 import InputUI from "../shared/InputUI";
 import style from "./SearchBar.module.scss";
 
-function SearchBar() {
+function SearchBar(props) {
+  const { distance, duration, sna, sbi, bemp, updateTime } = props;
   return (
     <>
       <div className={style.container}>
@@ -16,10 +20,25 @@ function SearchBar() {
             />
           </div>
           <div className={style.buttonBox}>
-            <ButtonUI text="搜索" btnStyle="btn__pill"></ButtonUI>
+            <ButtonUI text="GO" btnStyle="btn__pill"></ButtonUI>
           </div>
         </div>
-
+        <div className={style.info__route}>
+          <div className={style.route__distance}>Distance:{distance}</div>
+          <div className={style.route__duration}>Duration:{duration}</div>
+        </div>
+        {/* 資訊欄必須要點擊才會出現，後續要補判斷式 */}
+        <div className={style.info__station}>
+          <h3 className={style.station__title}>站點資訊</h3>
+          <div className={style.station__text}>站點名稱：{sna}</div>
+          <div className={style.station__text}>可借車輛：{sbi}</div>
+          <div className={style.station__text}>可停空位：{bemp}</div>
+          <div className={style.station__text}>更新時間：{updateTime}</div>
+        </div>
+        <div className={style.action}>
+          <RiDeleteBin6Line className={style.riDelete} size={35} />
+          <BiNavigation className={style.biNav} size={35} />
+        </div>
         {/* 資訊顯示欄位並附上刪除資料按鈕 */}
       </div>
     </>
