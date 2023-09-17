7b5e4f6183770bf668f3bdad77e7216b206ad892
jeff0518 <cycivs07@icloud.com>
2023-09-18 00:29:25 +0800

    feat: 自定義hooks

--- pages/bike/index.jsx
+++ pages/bike/index.jsx
@@ -1,8 +1,15 @@
+import { useGoogleMaps } from "@/utils/useGoogleMaps";
+
 import Map from "@/components/map/Map";
 import SearchBar from "@/components/map/SearchBar";
+import ErrorModel from "@/components/error/ErrorModel";
 import style from "./index.module.scss";
 
 function BikePage() {
+  const { isLoaded } = useGoogleMaps();
+
+  if (!isLoaded) return <ErrorModel errorName="loading" />;
+
   return (
     <div className={style.container}>
       <div className={style.container__search}>
